ef2a3c4847174795447ba1cf5840c2f99c109d84
Towbee05 <olatiseoluwatobiloba@gmail.com>
2024-02-06 19:36:23 -1200

    First pushed message

---
+++ NewConvert/signup/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def signup(request):
+    return render(request, "signup/index.html", context = {})
+
+def convert(request):
+    if request.method == "POST":
+        Fullname = request.POST["fullname"]
+        Email = request.POST["email"]
+        Phone = request.POST["phone_number"]
+        Address = request.POST["address"]
+        Prayer = request.POST["prayer"]
+        print(Fullname)
+        print(Email)
+        print(Phone)
+        print(Address)
+        print(Prayer)
+    return render (request, "signup/convert.html", context = {})
