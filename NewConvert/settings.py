9de55219bf19e09a282989887c310f54b0301548
luvakin <akinwalearemu0@gmail.com>
2025-10-24 09:26:20 +0100

    message'

--- NewConvert/settings.py
+++ NewConvert/settings.py
@@ -33,7 +33,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = "django-insecure-3x&jc$2$i+=lo1&%$lznch6oe*(9zcl3p134pjm_f%crddk2wm"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [ "168.231.114.107", "rccgbtc.com", "www.rccgbtc.com" ,"127.0.0.1"]
 
