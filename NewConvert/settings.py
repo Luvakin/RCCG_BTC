fd44d98fb02ecc1a33e95d38f2904d48d6b18060
luvakin <akinwalearemu0@gmail.com>
2026-01-11 08:02:16 +0100

    message

--- NewConvert/settings.py
+++ NewConvert/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = "django-insecure-3x&jc$2$i+=lo1&%$lznch6oe*(9zcl3p134pjm_f%crddk2wm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [ "168.231.114.107", "rccgbtc.com", "www.rccgbtc.com" ,"127.0.0.1", "rccg-btc.onrender.com"]
+ALLOWED_HOSTS = ["127.0.0.1", "rccg-btc.onrender.com"]
 
 
 # Application definition
